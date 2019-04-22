fab14fcaccb89fcdcda9acee42f6c3ff0d0bce70
Anna Pawl <annampawl@gmail.com>
2019-04-22 15:55:36 -0700

    boilerplate pages

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { directive } from '@babel/types';
+
+const About = (props) => {
+    return (
+        <div>
+            <h1>
+                The About Page!
+            </h1>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Resume = (props) => {
+  return (
+    <div>
+      <h1>
+        The Resume Page!
+      </h1>
+    </div>
+  )
+}
+
+export default Resume
