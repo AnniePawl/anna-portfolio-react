cf810bb43c386a50dec13e11184d52e80e206b4d
Anna Pawl <annampawl@gmail.com>
2019-11-02 14:07:50 -0700

    the latest

--- src/App.js
+++ src/App.js
@@ -5,27 +5,26 @@ import './App.css'
 import NavBar from './NavBar/NavBar'
 import Home from './Home/Home';
 import About from './About/About'
-import Contact from './Contact/Contact'
 import TechPortfolio from './TechPortfolio/TechPortfolio'
 import CreativePortfolio from './CreativePortfolio/CreativePortfolio'
 import WritingPortfolio from './WritingPortfolio/WritingPortfolio'
-import Footer from './Footer/Footer'
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <h1>It's working</h1>
         <NavBar />
         <Route path="/" exact component={Home} />
         <Route path="/About" component={About} />
-        <Route path="/Contact" component={Contact} />
+
         <Route path="/TechPortfolio" component={TechPortfolio} />
         <Route path="/WritingPortfolio" component={WritingPortfolio} />
         <Route path="/CreativePortfolio" component={CreativePortfolio} />
-        {/* Should footer haver route? */}
-        {/* <Route path="/Footer" component={Footer} /> */}
 
-        <Footer />
+
+
       </div>
     </BrowserRouter >
   );
