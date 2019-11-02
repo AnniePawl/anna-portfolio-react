3c40d659a765cc2c00112761f2d5e6aae10c9e0c
Anna Pawl <annampawl@gmail.com>
2019-11-02 15:36:51 -0700

    custom home nav in progress

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,83 @@
 import React from 'react'
 import './Home.css'
 
-const Home = (props) => {
+function Home() {
   return (
     <div>
-      <h1 className="home__title">
-        This is the Home Page!
-      </h1>
+      <h1>Home</h1>
+      <div home_bottom_container>
 
+
+        <div className='home_bottom_rectangle'>
+
+          {/* HOME CIRCLES Full  */}
+          <div className='home_circles'>
+
+            {/* CIRCLE ONE - CSS ANIMATIONS */}
+            <div className='home_circle home_circle1'>
+            </div>
+
+            {/* CIRCLE TWO */}
+            <div className='home_circle home_circle2'>
+            </div>
+
+            {/* CIRCLE THREE */}
+            <div className='home_circle home_circle3'>
+
+            </div>
+
+
+            {/* CIRCLE FOUR */}
+            <div className='home_circle home_circle4'>
+
+            </div>
+
+            {/* CIRCLE FIVE */}
+            <div className='home_circle home_circle5'>
+
+            </div>
+
+
+
+          </div>
+
+        </div>
+
+
+        {/* RED HALF CIRCLES */}
+        <div className='home_half_circles'>
+          {/* Circle 1 */}
+          <div className='half_circle'>
+            <div className='inner_circle'></div>
+          </div>
+
+          {/* Circle 2 */}
+          <div className='half_circle'>
+            <div className='inner_circle'></div>
+          </div>
+
+          {/* Circle 3 */}
+          <div className='half_circle'>
+            <div className='inner_circle'></div>
+          </div>
+
+          {/* Cirlce 4 */}
+          <div className='half_circle'>
+            <div className='inner_circle'></div>
+          </div>
+
+          {/* Circle 5 */}
+          <div className='half_circle'>
+            <div className='inner_circle'></div>
+          </div>
+
+        </div>
+
+
+      </div>
     </div>
+
   )
 }
-
 
 export default Home
