faf15cac2bd43ae58ebb390a145168e0dca9c7d4
Anna Pawl <annampawl@gmail.com>
2019-11-02 16:13:47 -0700

    home button animations

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <h1>Home</h1>
       <div home_bottom_container>
 
@@ -46,36 +46,47 @@ function Home() {
 
         {/* RED HALF CIRCLES */}
         <div className='home_half_circles'>
+
           {/* Circle 1 */}
           <div className='half_circle'>
-            <div className='inner_circle'></div>
+            <div className='inner_circle'>
+              <h1 className='home_text'> Pure Css<br /> Animations</h1>
+            </div>
           </div>
 
           {/* Circle 2 */}
           <div className='half_circle'>
-            <div className='inner_circle'></div>
+            <div className='inner_circle'>
+              <h1 className='home_text'> Technical<br />Projects</h1>
+            </div>
           </div>
 
           {/* Circle 3 */}
           <div className='half_circle'>
-            <div className='inner_circle'></div>
+            <div className='inner_circle'>
+              <h1 className='home_text'> Design/<br /> UX/UI</h1>
+            </div>
           </div>
 
           {/* Cirlce 4 */}
           <div className='half_circle'>
-            <div className='inner_circle'></div>
+            <div className='inner_circle'>
+              <h1 className='home_text'>Written<br />Works</h1>
+            </div>
           </div>
 
           {/* Circle 5 */}
           <div className='half_circle'>
-            <div className='inner_circle'></div>
+            <div className='inner_circle'>
+              <h1 className='home_text'> About/<br /> Contact</h1>
+            </div>
           </div>
 
         </div>
 
 
       </div>
-    </div>
+    </div >
 
   )
 }
