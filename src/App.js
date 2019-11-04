4df325649e5f457b5ef6edd59e1adcde9abddc22
Anna Pawl <annampawl@gmail.com>
2019-11-04 15:31:47 -0800

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 // IMPORTS
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './Home/Home'
 import './App.css'
-import NavBar from './NavBar/NavBar'
-import Home from './Home/Home';
-import About from './About/About'
-import TechPortfolio from './TechPortfolio/TechPortfolio'
-import CreativePortfolio from './CreativePortfolio/CreativePortfolio'
-import WritingPortfolio from './WritingPortfolio/WritingPortfolio'
+
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <div className="App">
+      <BrowserRouter>
+
         <h1>It's working</h1>
-        <NavBar />
-        <Route path="/" exact component={Home} />
-        <Route path="/About" component={About} />
+        <Home />
 
-        <Route path="/TechPortfolio" component={TechPortfolio} />
-        <Route path="/WritingPortfolio" component={WritingPortfolio} />
-        <Route path="/CreativePortfolio" component={CreativePortfolio} />
+        {/* <Route path="/" exact component={Home} /> */}
+        {/* <Route path="/About" component={About} /> */}
 
-
-
-      </div>
-    </BrowserRouter >
+      </BrowserRouter >
+    </div>
   );
 }
 
