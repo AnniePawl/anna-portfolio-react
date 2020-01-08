41225f3651c46fe504e7ea5a57061bdc539abb87
Anna Pawl <annampawl@gmail.com>
2020-01-08 10:49:48 -0800

    link to external animations portfiolo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home/Home'
+import PureCssAnimations from './components/PureCssAnimations/PureCssAnimations'
 import About from './components/About/About'
 import './App.css'
 
@@ -11,13 +12,16 @@ function App() {
     <div className="App">
       <Router>
 
+        {/* Home Route */}
         <Route path="/" >
           <Home />
         </Route>
 
-
-        {/* <Route path="/About" component={About} ></Route> */}
-
+        {/* CSS Animation Route */}
+        <Route path='/css-animations' component={() => {
+          window.location.href = 'https://anniepawl.github.io/Pure-CSS-Animations/';
+          return null;
+        }} />
       </Router >
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,68 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import Pom from './Pom/Pom'
+import Pom from '../../assets/pomegranate/Pom'
 import './styles/Home.scss'
 
 class Home extends React.Component {
   render() {
     return (
+
       <div>
         <h1>Home</h1>
-        {/* Pom div to CSS Animations */}
-        <div>
-          <Link to="/">
-            <Pom />
-          </Link>
 
+        <div className='home_container'>
+          {/* About Me */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>About Me</h1>
+          </div>
+
+          {/* CSS ANIMATIONS */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>CSS Animations</h1>
+          </div>
+
+          {/* Technical Projects */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>Technical Projects</h1>
+          </div>
+
+
+          {/* Written Work */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>Written Work</h1>
+          </div>
+
+
+          {/* Graphic Desin */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>Graphic Design</h1>
+          </div>
+
+          {/* Embroidery */}
+          <div>
+            <Link to="/">
+              <Pom />
+            </Link>
+            <h1>Embroidery</h1>
+          </div>
         </div>
+
 
       </div>
     )
