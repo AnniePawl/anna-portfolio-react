cf413c69b3917cf3df788a788a79e461944bfc32
Anna Pawl <annampawl@gmail.com>
2020-01-15 13:42:29 -0800

    refactored bridge and fog

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import Pom from '../../assets/pomegranate/Pom'
-import TechnicalProjects from '../TechnicalProjects/TechnicalProjects'
-import WrittenWork from '../WrittenWork/WrittenWork'
 import './styles/Home.scss'
 import Navbar from '../Navbar/Navbar'
 import Fog from './Fog/Fog'
-import sf from '../../assets/sf.svg'
+
 
 class Home extends React.Component {
   render() {
@@ -15,26 +10,7 @@ class Home extends React.Component {
 
       <div>
         <Navbar />
-
         <Fog />
-        <div className='home__container'>
-
-
-
-
-
-          {/* Technical Projects Section  */}
-          {/* <div>
-            < TechnicalProjects />
-          </div> */}
-
-          {/* Writing Portfolio Section */}
-          {/* <div>
-            <WrittenWork />
-          </div> */}
-        </div>
-
-
       </div>
     )
   }
