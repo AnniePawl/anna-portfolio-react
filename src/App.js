7c57e7f92b2cb5f249091a02c1f850f2863b4609
Anna Pawl <annampawl@gmail.com>
2020-03-27 12:47:44 -0400

    refactoring navbar w/react router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 // IMPORTS
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './components/Home/Home'
-import PureCssAnimations from './components/PureCssAnimations/PureCssAnimations'
-import About from './components/About/About'
-import './App.css'
-
+import Home from "./components/Home/Home";
+import TechnicalProjects from "./components/TechnicalProjects/TechnicalProjects";
+import CreativePortfolio from "./components/CreativePortfolio/CreativePortfolio";
 
 function App() {
   return (
     <div className="App">
       <Router>
-
         {/* Home Route */}
-        <Route path="/" >
+        <Route path="/">
           <Home />
         </Route>
 
-        {/* Code Route */}
-        {/* <Route path='/code' component={() => {
-          window.open(window.href = 'https://github.com/AnniePawl');
-          return null;
-        }} /> */}
+        {/* Technical Projects */}
+        <Route path="/technical-projects">
+          <TechnicalProjects />
+        </Route>
+
+        {/* Art/Design Route */}
+        <Route path="/art-design">
+          <CreativePortfolio />
+        </Route>
 
         {/* CSS Animation Route */}
         {/* <Route path='/css-animations' component={() => {
           window.open(window.href = 'https://anniepawl.github.io/Pure-CSS-Animations/');
           return null;
         }} /> */}
-
-
-      </Router >
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
-import './styles/Navbar.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import "./styles/Navbar.scss";
 
 function Navbar() {
   return (
+    <div className="navbar">
+      <ul>
+        {/* Code: Link to GH*/}
+        <li>
+          <Link to="/technical-projects">Technical Projects</Link>
+          {/* <a target="blank" href="https://github.com/AnniePawl">
+            Technical Projects
+          </a> */}
+        </li>
 
-    <div className="nav">
-      <nav className='navbar'>
-        <ul>
+        {/* Graphic Design */}
+        <li>
+          <Link className="navbar__link" to="/art-design">
+            Art | Design
+          </Link>
+        </li>
 
-          {/* Code: Link to GH*/}
-          <li>
-            <a target='blank' href='https://github.com/AnniePawl'>Code</a>
-          </li>
+        {/* CSS Animations: Link to GH Page */}
+        {/* <li>
+          <a
+            target="blank"
+            href="https://anniepawl.github.io/Pure-CSS-Animations/"
+          >
+            CSS Animations
+          </a>
+        </li> */}
 
-          {/* CSS Animations: Link to GH Page */}
-          <li>
-            <a target='blank' href='https://anniepawl.github.io/Pure-CSS-Animations/'>CSS Animations</a>
-          </li>
+        {/* Writing*/}
+        <li>
+          <a target="blank" href="https://medium.com/@annapawl">
+            Writing
+          </a>
+        </li>
 
-          {/* Graphic Design */}
-          <li>
-            <Link className="navbar__link" to="/">Graphic Design</Link>
-          </li>
-
-          {/* Writing*/}
-          <li>
-            <a target='blank' href='https://medium.com/@annapawl'>Writing</a>
-          </li>
-
-          {/* Emrbroidery*/}
-          <li>
-            <Link className="navbar__link" to="/">Embroidery</Link>
-          </li>
-
-        </ul>
-      </nav>
-
-    </div >
-
-  )
+        {/* Emrbroidery*/}
+        {/* <li>
+          <Link className="navbar__link" to="/">
+            Embroidery
+          </Link>
+        </li> */}
+      </ul>
+    </div>
+  );
 }
 
-export default withRouter(Navbar)
-
+export default withRouter(Navbar);
