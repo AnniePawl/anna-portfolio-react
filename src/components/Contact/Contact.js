fefeba678eb3d7af33f9bb7fb567b8e34d678c73
Anna Pawl <annampawl@gmail.com>
2020-03-29 16:05:35 -0400

    added custom social media buttons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Buttons from "./Buttons";
 import "./styles/Contact.scss";
 
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div className="contact-container">
+      {/* <h1>Contact</h1> */}
+      <div clasName="button-container">
+        <Buttons />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
