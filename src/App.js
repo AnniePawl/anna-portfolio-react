47792d3ceff936f1f17474ca2a29bcd93648e951
Anna Pawl <annampawl@gmail.com>
2020-07-03 16:24:20 -0400

    prepparing for gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // IMPORTS
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import LandingPage from "../src/components/LandingPage/LandingPage";
-import Portfolio from "../src/components/Portfolio/Portfolio";
-import "./app.scss";
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import LandingPage from '../src/components/LandingPage/LandingPage';
+import Portfolio from '../src/components/Portfolio/Portfolio';
+import './app.scss';
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        {/* If the current URL is /about, this route is rendered
+	return (
+		<div className="App">
+			<Router>
+				{/* If the current URL is /about, this route is rendered
             while the rest are ignored */}
 
-        {/* Home Route */}
-        <Route path="/" exact component={LandingPage} />
+				{/* Home Route */}
+				<Route path="/" exact component={LandingPage} />
 
-        {/* Portfolio Route */}
-        <Route path="/portfolio/" component={Portfolio} />
-      </Router>
-    </div>
-  );
+				{/* Portfolio Route */}
+				<Route path="/portfolio/" component={Portfolio} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
