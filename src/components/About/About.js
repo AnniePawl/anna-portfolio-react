f04fe9c32c0f127bb0109e0a494675fdd30b6364
Anna Pawl <annampawl@gmail.com>
2020-07-17 11:43:16 -0400

    updating home styles, merging portfolio changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,19 @@
-import React from "react";
-import "./styles/About.scss";
-import Bridge from "./Bridge/Bridge";
+import React from 'react';
+import './styles/About.scss';
+import Bridge from './Bridge/Bridge';
 
 function About() {
-  return (
-    <div className="about-container">
-      <div className="about-content">
-        <h1 className="about-heading">About</h1>
-        <p>
-          I am a software engineer and designer, living and working in San
-          Francisco.{" "}
-        </p>
-      </div>
+	return (
+		<div className="about__container">
+			<div className="about__content">
+				<h1 className="about__heading">About</h1>
+				<p>I am a software engineer and designer, living and working in San Francisco. </p>
+			</div>
 
-      <div className="bridge-container">
-        <Bridge />
-      </div>
-    </div>
-  );
+			<div className="bridge-container">
+				<Bridge />
+			</div>
+		</div>
+	);
 }
 export default About;
